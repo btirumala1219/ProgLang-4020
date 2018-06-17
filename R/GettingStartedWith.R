0e56d06ab1a3223b8ffea5cbd9b93d241227a90f
btirumala <barath.tirumala@gmail.com>
2018-06-17 19:05:22 -0400

    finished assignment 1

--- R/GettingStartedWith.R
+++ R/GettingStartedWith.R
@@ -1,5 +1,39 @@
-MyData <- read.csv(file="Rdata.csv", header=TRUE, sep=",")
-june <- subset(MyData, Month == 6)
-div2 <- subset(june, Day%%2 == 0)
-wind <- subset(div2, Wind>9)
-ans <- mean(wind$Wind,na.rm = TRUE)
+RAssignment1Problem1 <- function()
+{
+  MyData <- read.csv(file="Rdata.csv", header=TRUE, sep=",")
+  june <- subset(MyData, Month == 6)
+  div2 <- subset(june, Day%%2 == 0)
+  wind <- subset(div2, Wind>9)
+  ans <- mean(wind$Wind,na.rm = TRUE)
+  ans
+}
+
+RAssignment1Problem2 <- function()
+{
+  MyData <- read.csv(file="Rdata.csv", header=TRUE, sep=",")
+  exist <- complete.cases(MyData)
+  fin <- MyData[exist, ]
+
+  minO <- min(fin$Ozone)
+  maxO <- max(fin$Ozone)
+
+  minS <- min(fin$Solar.R)
+  maxS <- max(fin$Solar.R)
+
+  minW <- min(fin$Wind)
+  maxW <- max(fin$Wind)
+
+  minT <- min(fin$Temp)
+  maxT <- max(fin$Temp)
+
+  x <- 300
+
+  Ozone <- c(minO, maxO)
+  Solar.R <- c(minS, maxS)
+  Wind <- c(minW, maxW)
+  Temp <- c(minT, maxT)
+
+  ans <- data.frame(Ozone, Solar.R, Wind, Temp)
+
+  ans
+}
--- R/StartR.R
+++ R/StartR.R
@@ -1,1 +1,25 @@
 MyData <- read.csv(file="Rdata.csv", header=TRUE, sep=",")
+exist <- complete.cases(MyData)
+fin <- MyData[exist, ]
+
+minO <- min(fin$Ozone)
+maxO <- max(fin$Ozone)
+
+minS <- min(fin$Solar.R)
+maxS <- max(fin$Solar.R)
+
+minW <- min(fin$Wind)
+maxW <- max(fin$Wind)
+
+minT <- min(fin$Temp)
+maxT <- max(fin$Temp)
+
+x <- 300 
+
+Ozone <- c(minO, maxO)
+Solar.R <- c(minS, maxS)
+Wind <- c(minW, maxW)
+Temp <- c(minT, maxT)
+
+ans <- data.frame(Ozone, Solar.R, Wind, Temp)
+
